9d0b6961e6f4899be71dd1e0db4b13abf1a55fb5
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-06-26 23:28:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kalanakt/baesuzy-v.2.0.git /baesuzy-v.2.0
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /baesuzy-v.2.0
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /baesuzy-v.2.0
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
