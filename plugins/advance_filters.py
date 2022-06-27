a77861b3f44e47a9f38193c05a494c886fd9fbf0
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-06-27 11:19:47 +0530

    Create advance_filters.py

---
+++ plugins/advance_filters.py
@@ -0,0 +1,44 @@
+namelist = ('you','strangerthings','tedlasso','peakyblinders','allofusaredead','cobrakai','vikings')
+
+linklist = [
+    [
+        ['e7','you720e1','you720e2','you720e3'],
+        ['e108','you1080e1','you1080e2','you1080e3'],
+        ['h7','you720h1','you720h1','you720h1']
+    ],
+    [
+        ['e4','strangerthings480e1','strangerthings480e2','strangerthings480e3', 'strangerthings480e4'],
+        ['h4','strangerthings480h1','strangerthings480h2','strangerthings480h3', 'strangerthings480h4'],
+        ['e7','strangerthings720e1','strangerthings720e2','strangerthings720e3', 'strangerthings720e4'],
+        ['h7','strangerthings720h1','strangerthings720h2','strangerthings720h3','strangerthings720h4'],
+        ['e108','strangerthings1080e1','strangerthings1080e2','strangerthings1080e3', 'strangerthings1080e4']
+    ],
+    [
+        ['e7','tedlasso720e1','tedlasso720e2']
+    ],
+    [
+        ['e7','peakyblinders720e1','peakyblinders720e2','peakyblinders720e3','peakyblinders720e4','peakyblinders720e5','peakyblinders720e6'],
+        ['e108','peakyblinders1080e1','peakyblinders1080e2','peakyblinders1080e3','peakyblinders1080e4','peakyblinders1080e5','peakyblinders1080e6']
+    ],
+    [
+        ['e4','allofusaredead480e1'],
+        ['e7','allofusaredead720e1'],
+        ['e108','allofusaredead1080e1'],
+        ['k7','allofusaredead720k1'],
+        ['k108','allofusaredead1080k1'],
+        ['h7','allofusaredead720h1']
+    ],
+    [
+        ['e4','cobrakai480e1', 'cobrakai480e2', 'cobrakai480e3', 'cobrakai480e4'],
+        ['e7','cobrakai720e1', 'cobrakai720e2', 'cobrakai720e3', 'cobrakai720e4'],
+        ['h7','cobrakai720h1', 'cobrakai720h2', 'cobrakai720h3', 'cobrakai720h4'],
+        ['e108','cobrakai1080e1', 'cobrakai1080e2', 'cobrakai1080e3', 'cobrakai1080e4']
+    ],
+    [
+        ['e4','vikings480e1','vikings480e2','vikings480e3','vikings480e4','vikings480e5','vikings480e6'],
+        ['e7','vikings720e1','vikings720e2','vikings720e3','vikings720e4','vikings720e5','vikings720e6'],
+        ['e108','vikings1080e1','vikings1080e2','vikings1080e3','vikings1080e4','vikings1080e5','vikings1080e6'],
+        ['h4','vikings480h1','vikings480h2','vikings480h3','vikings480h4','vikings480h5','vikings480h6'],
+        ['h7','vikings720h1','vikings720h2','vikings720h3','vikings720h4','vikings720h5','vikings720h6']
+    ]
+]
