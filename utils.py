291ada8c086e8c432f517ab0a1fa586fa2a97637
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-05 03:16:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -287,23 +287,20 @@ def get_name(name):
 
 async def get_url(fileid):
     ident, file_id = fileid.split("#")
-    #urllink = f'https://playdisk.xyz/st?api=3ba547cddecb2156a75b2ab37c9fecdbf5655d7f&url=https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'
-    #urllink = f'https://www.iamkt.xyz/st?api=41bd4ad28cde15c72c1baa6d16f05577cee0a90f&url=https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'
-    #urllink = f'https://shorturllink.in/st?api=3ef6a62253efbe7a63dd29201b2f9c661bd15795&url=https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'
     link = f"https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}"
     api = "41bd4ad28cde15c72c1baa6d16f05577cee0a90f"
     url = f'https://shorturljatt.xyz/api'
     params = {
-              'api': api,
-              'url': link
-             }
+        'api': api,
+        'url': link
+    }
 
-   async with aiohttp.ClientSession() as app:
-    async with app.get(url, params=params, raise_for_status=True, ssl=False) as results:
-        data = await results.json()
-        if data["status"] == "success":
-            urllink = data['shortenedUrl']
-            return urllink
+    async with aiohttp.ClientSession() as app:
+        async with app.get(url, params=params, raise_for_status=True, ssl=False) as results:
+            data = await results.json()
+            if data["status"] == "success":
+                urllink = data['shortenedUrl']
+                return urllink
 
 def getseries(name):
     name = name.lower()
