0dec417748d0cc8f2bd108b39a22b0045331ae07
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-05 04:17:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,8 +16,8 @@ from plugins.advance_filters import namelist, linklist
 from Sewlink import *
 from bs4 import BeautifulSoup
 import requests
-# import aiohttp
-# import json
+import json
+import aiohttp
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -288,29 +288,18 @@ def get_name(name):
 
 def get_url(fileid):
     ident, file_id = fileid.split("#")
-    #urllink = f'https://playdisk.xyz/st?api=3ba547cddecb2156a75b2ab37c9fecdbf5655d7f&url=https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'
-    urllink = f'https://shorturllink.in/st?api=3ef6a62253efbe7a63dd29201b2f9c661bd15795&url=https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'   
-    urllink = url_shortener.tinyurl.short(urllink)
-    return urllink
-
-# async def get_url(fileid):
-#     ident, file_id = fileid.split("#")
-#     link = f"https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}"
-#     api = "41bd4ad28cde15c72c1baa6d16f05577cee0a90f"
-#     url = f'https://www.iamkt.xyz/api'
-#     params = {
-#         'api': api,
-#         'url': link
-#     }
-#     return url
-#     async with aiohttp.ClientSession() as app:
-#         async with app.get(url, params=params, raise_for_status=True, ssl=False) as results:
-#             data = await results.json()
-#             if data["status"] == "success":
-#                 urllink = data['shortenedUrl']
-#                 logging.info(f"{urllink} - 1se")
-#             else:
-#                 logging.info(f"{urllink} - PeerIdInvalid")
+    urllink = f'https://telegram.dog/SpaciousUniverseBot?start={ident}_{file_id}'   
+    api = "41bd4ad28cde15c72c1baa6d16f05577cee0a90f"
+    url = "https://iamkt.xyz/api"
+    params = {'api': api,
+                'url': urllink
+              }
+    async with aiohttp.ClientSession() as app:
+        async with app.get(url, params=params, raise_for_status=True, ssl=False) as results:
+          data = await results.json()
+          if data["status"] == "success":
+              return data['shortenedUrl']
+          
 
 def getseries(name):
     name = name.lower()
