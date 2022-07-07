9764f355bff7450ee4731779b0b83ba119b58f71
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-07 22:31:09 +0530

    Update tvseriesfilters.py

--- database/tvseriesfilters.py
+++ database/tvseriesfilters.py
@@ -7,9 +7,9 @@ logger.setLevel(logging.ERROR)
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
-async def add_tvseries_filter(name, language, seasonlink): 
+async def add_tvseries_filter(name, language, quality, seasonlink): 
     mycol = mydb["tvseries"]
-    mydict = { "name": str(name), "language": str(language), "seasonlink": str(seasonlink) }
+    mydict = { "name": str(name), "language": str(language),"quality": str(quality), "seasonlink": str(seasonlink) }
     
     try:
         mycol.insert_one(mydict)
@@ -19,12 +19,12 @@ async def add_tvseries_filter(name, language, seasonlink):
 async def update_tvseries_filter(name, language, seasonlink):
     mycol = mydb["tvseries"]
     filter = { 'name': str(name) }
-    newvalues = { "$set": { "language": str(language), "seasonlink": str(seasonlink) } } 
+    newvalues = { "$set": { "language": str(language), "quality": str(quality), "seasonlink": str(seasonlink) } } 
     mycol.update_one(filter, newvalues)
 
 async def getlinks(name)
     mycol = mydb["tvseries"]
-    for x in mycol.find({},{ "_id": 0, "name": 1, "language": 1, "seasonlink": 1):
+    for x in mycol.find({},{ "_id": 0, "name": 1, "language": 1, "quality": 1,"seasonlink": 1):
         return x  
 
 async def find_tvseries_filter(name): 
