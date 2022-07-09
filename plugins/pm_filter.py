bc67000add0a3857c10a639ed5c4d88e3a25128c
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-09 15:51:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,8 +37,8 @@ SPELL_CHECK = {}
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
-    t = await tvseries_filters(client, message)
-    if k == False & t == False:
+    #t = await tvseries_filters(client, message)
+    if k == False:
         await auto_filter(client, message)
 #         t = await tvseries_filters(client, message)
 #         if not t:
