f0beee26f7d450db5b0b32d2a03000a7a4bb29de
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-10 19:58:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -145,9 +145,13 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase')
-            await asyncio.sleep(10)
-            await k.delete()
+            try:
+                k = await query.message.edit('This Movie Not Found In DataBase')
+                await asyncio.sleep(10)
+                await k.delete()
+                
+            except:
+                return
 
 @Client.on_message(filters.command("addseries") & filters.incoming & ~filters.edited)
 async def tvseries_adder(bot, message):
