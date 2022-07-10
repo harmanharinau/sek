0adc71d72b3b2c87bd8f2defc4f4f4e7dd054c7f
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-11 01:51:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -80,6 +80,9 @@ async def start(client, message):
             	btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
             except IndexError:
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}/{message.command[1]}")])
+            except:
+                return 
+            
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
