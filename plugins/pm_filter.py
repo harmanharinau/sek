4a0042f4e49bd727a82ce062473eef99f5187b06
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-11 01:17:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -871,50 +871,50 @@ async def manual_filters(client, message, text=False):
 async def tvseries_filters(client, message, text=False):
     name = getseries(message.text)
     seriess = await find_tvseries_filter(name)
-  
-    if seriess:
-        for series in seriess:
-            language = series['language']
-            quality = series['quality']
-            links = series['seasonlink']
-            links = links.split(",")
-            btns = [
-                [
-                    InlineKeyboardButton(
-                        text=f'Season {links.index(link)+1}', url = gen_url(link)
-                    ),
-                ]
-                for link in links
-            ]
-            btns.insert(0, 
-                [InlineKeyboardButton(text=f"{language} - {quality}", callback_data="pages")]
-            )
-            imdb = await get_poster(message.text) if IMDB else None
-            if imdb:
-                cap = IMDB_TEMPLATE.format(
-                    title = imdb['title'],
-                    votes = imdb['votes'],
-                    year = imdb['year'],
-                    genres = imdb['genres'],
-                    poster = imdb['poster'],
-                    plot = imdb['plot'],
-                    rating = imdb['rating'],
-                    url = imdb['url']
-                )
+    await message.reply_text(seriess)
+#     if seriess:
+#         for series in seriess:
+#             language = series['language']
+#             quality = series['quality']
+#             links = series['seasonlink']
+#             links = links.split(",")
+#             btns = [
+#                 [
+#                     InlineKeyboardButton(
+#                         text=f'Season {links.index(link)+1}', url = gen_url(link)
+#                     ),
+#                 ]
+#                 for link in links
+#             ]
+#             btns.insert(0, 
+#                 [InlineKeyboardButton(text=f"{language} - {quality}", callback_data="pages")]
+#             )
+#             imdb = await get_poster(message.text) if IMDB else None
+#             if imdb:
+#                 cap = IMDB_TEMPLATE.format(
+#                     title = imdb['title'],
+#                     votes = imdb['votes'],
+#                     year = imdb['year'],
+#                     genres = imdb['genres'],
+#                     poster = imdb['poster'],
+#                     plot = imdb['plot'],
+#                     rating = imdb['rating'],
+#                     url = imdb['url']
+#                 )
 
-                try:
-                    await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btns))
-                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-                    pic = imdb.get('poster')
-                    poster = pic.replace('.jpg', "._V1_UX360.jpg")
-                    await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btns))
-                except Exception as e:
-                    logger.exception(e)
-                    cap = f"Here is what i found for your Request"
-                    await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btns))
-            else:
-                cap = f"Here is what i found for your Request"
-                await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btns))
+#                 try:
+#                     await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btns))
+#                 except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+#                     pic = imdb.get('poster')
+#                     poster = pic.replace('.jpg', "._V1_UX360.jpg")
+#                     await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btns))
+#                 except Exception as e:
+#                     logger.exception(e)
+#                     cap = f"Here is what i found for your Request"
+#                     await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btns))
+#             else:
+#                 cap = f"Here is what i found for your Request"
+#                 await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btns))
   
-    else:
-        return False
+#     else:
+#         return False
