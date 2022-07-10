7b57d127f6aa72b78360898438673ccbc284e1e6
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-10 19:11:33 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -61,9 +61,11 @@ async def send_for_index(bot, message):
         last_msg_id = int(match.group(5))
         if chat_id.isnumeric():
             chat_id  = int(("-100" + chat_id))
-    elif message.forward_from_chat.type == 'channel':
-        last_msg_id = message.forward_from_message_id
-        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
+            
+    elif message.forward_from_chat.type:
+        if message.forward_from_chat.type == 'channel':
+            last_msg_id = message.forward_from_message_id
+            chat_id = message.forward_from_chat.username or message.forward_from_chat.id
     else:
         return
     try:
