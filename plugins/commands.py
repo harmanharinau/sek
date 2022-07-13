f44f3813b69a9cb78f097b7188aa48441eb8d991
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-14 02:13:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -254,7 +254,7 @@ async def start(client, message):
         protect_content=True if pre == 'filep' else False,
         )
     
-    files = await send_more_files(title)
+    files = await send_more_files(f_caption)
     
     if files == "Northing Found":
         await message.reply(files)
