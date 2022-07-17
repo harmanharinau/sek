35118f62ea5e2b33b21c5473efe25d854828e84a
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-17 20:06:06 +0530

    Create quickdb.py

---
+++ database/quickdb.py
@@ -0,0 +1,34 @@
+import pymongo
+from info import DATABASE_URI, DATABASE_NAME
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+myclient = pymongo.MongoClient(DATABASE_URI)
+mydb = myclient[DATABASE_NAME]
+
+async def add_inst_filter(fileid, links): 
+    mycol = mydb["quickdb"]
+    mydict = {"fileid": str(fileid), "links": str(links) }
+    
+    try:
+        x = mycol.insert_one(mydict)
+    except:
+        logger.exception('Some error occured!', exc_info=True)
+                
+async def remove_inst(fileid):
+    mycol = mydb["quickdb"]
+    myquery = { "fileid": str(fileid) }
+    mycol.delete_one(myquery)
+    
+async def get_ids(fileid):
+    mycol = mydb["quickdb"]
+    myquery = { "fileid": str(fileid) }
+    mydoc = mycol.find(myquery)
+    for x in mydoc:
+      return x 
+
+async def get(id):
+    mycol = mydb["quickdb"]
+    for x in mycol.find():
+        return x
