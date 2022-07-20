e73ea466c3c966a52e7716f8381270d91ae86ca0
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-20 12:58:46 +0530

    try upts

--- plugins/status_editor.py
+++ plugins/status_editor.py
@@ -3,7 +3,6 @@ import time
 import logging
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
 from info import ADMINS
 from info import INDEX_REQ_CHANNEL as LOG_CHANNEL
@@ -20,36 +19,31 @@ logger.setLevel(logging.INFO)
 prev_day_total_users = 35531
 prev_day_total_files = 577448
 
-# @Client.on_message(filters.chat(-1001552600483) & filters.text)
-# async def statsch(bot, message):
-#     for file_type in ("document", "video", "audio"):
-#         media = getattr(message, file_type, None)
-#         if media is not None:
-#             break
-#     else:
-#         return
+btn = [
+    [
+        InlineKeyboardButton("Developer", url="https://github.com/kalanakt"),
+        InlineKeyboardButton("Join Group", url="https://t.me/TMWAD_Support")
+    ]
+]
 
-#     media.file_type = file_type
-#     media.caption = message.caption
-#     await save_file(media)
 
-# @Client.on_message(filters.command("updatech") & filters.user(ADMINS))
-# async def set_channel_ststs(client, message):
-#     k = message.reply_text("processing...")
-#     todaySentFiles = count_sent_files()
-#     total_users = await db.total_users_count()
-#     files = await Media.count_documents()
-#     todayUsers = total_users - prev_day_total_users
-#     todayFiles = files - prev_day_total_files
-#     t = time.localtime()
-#     current_time = time.strftime("%H:%M:%S", t)
+@Client.on_message(filters.chat('TMWAD'))
+async def stats_channel(client, message):
+    todaySentFiles = await count_sent_files()
+    total_users = await db.total_users_count()
+    files = await Media.count_documents()
+    todayUsers = total_users - prev_day_total_users
+    todayFiles = files - prev_day_total_files
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
 
-#     try:
-#         await client.edit_message_text(
-#             chat_id='TMWAD',
-#             message_id=int(241290),
-#             text=script.POST_TEXT.format(
-#                 todaySentFiles, todayUsers, todayFiles, total_users, files, current_time),
-#         )
-#     except:
-#         logger.exception('Some error occured!', exc_info=True)
+    try:
+        await client.edit_message_text(
+            chat_id=str('TMWAD'),
+            message_id=int(241290),
+            text=script.POST_TEXT.format(
+                todaySentFiles, todayUsers, todayFiles, total_users, files, current_time),
+            reply_markup=InlineKeyboardMarkup(btn),
+        )
+    except:
+        logger.exception('Some error occured!', exc_info=True)
