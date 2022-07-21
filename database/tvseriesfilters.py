d375de71a788f946ce2b3132c4cccfae2689789f
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-21 09:37:47 +0530

    db encha

--- database/tvseriesfilters.py
+++ database/tvseriesfilters.py
@@ -7,29 +7,35 @@ logger.setLevel(logging.ERROR)
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
-async def add_tvseries_filter(name, language, quality, seasonlink): 
+
+async def add_tvseries_filter(name, language, quality, seasonlink):
     mycol = mydb["tvseries"]
-    mydict = { "name": str(name), "language": str(language),"quality": str(quality), "seasonlink": str(seasonlink) }
-    
+    mydict = {"name": str(name), "language": str(
+        language), "quality": str(quality), "seasonlink": str(seasonlink)}
+
     try:
         mycol.insert_one(mydict)
     except:
         logger.exception('Some error occured!', exc_info=True)
 
+
 async def update_tvseries_filter(name, language, quality, seasonlink):
     mycol = mydb["tvseries"]
-    filter = { 'name': str(name) }
-    newvalues = { "$set": { "language": str(language), "quality": str(quality), "seasonlink": str(seasonlink) } }
+    filter = {'name': str(name)}
+    newvalues = {"$set": {"language": str(language), "quality": str(
+        quality), "seasonlink": str(seasonlink)}}
     try:
         mycol.update_one(filter, newvalues)
     except:
         logger.exception('Some error occured!', exc_info=True)
 
+
 async def remove_tvseries(name):
     mycol = mydb["tvseries"]
-    myquery = { 'name': str(name) }
+    myquery = {'name': str(name)}
     mycol.delete_one(myquery)
-    
+
+
 async def getlinks():
     mycol = mydb["tvseries"]
     list = []
@@ -39,16 +45,17 @@ async def getlinks():
 # async def getlinks(name):
 #     mycol = mydb["tvseries"]
 #     for x in mycol.find({},{ "_id": 0, "name": 1, "language": 1, "quality": 1,"seasonlink": 1}):
-#         return x  
+#         return x
 
-async def find_tvseries_filter(name): 
+
+async def find_tvseries_filter(name):
     mycol = mydb["tvseries"]
     list = []
-    for x in mycol.find({"name": str(name)}):
+    for x in mycol.find({"name": str(f'/{name}/')}):  # db.users.find({"name": /m/})
         list.append(x)
     return list
 #     myquery = { "name": str(name) }
-#     myquery = { "name": { "$regex": str(name) } } 
+#     myquery = { "name": { "$regex": str(name) } }
 #     mydoc = mycol.find(myquery)
 #     for x in mydoc:
 #         return x
