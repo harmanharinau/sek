af7a612f13c33d4c8b4771ef7cc0a1c257f168b0
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-23 17:31:46 +0530

    bug fix

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get(
-    "IMDB_TEMPLATE", "<b>🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10\n\n<a href='https: // kalanakt.github.io/projects/telegram/baesuzy /'>How To Verify !</a>")
+    "IMDB_TEMPLATE", "<b>🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10\n\n<a href='https://kalanakt.github.io/projects/telegram/baesuzy/'>How To Verify !</a>")
 LONG_IMDB_DESCRIPTION = is_enabled(
     environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
