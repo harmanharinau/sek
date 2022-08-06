866827ce3e5a184d214b1a60174d2c44361c7dc4
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-08-06 18:36:52 +0530

    Update for pyrogram v2.0.0

--- info.py
+++ info.py
@@ -32,10 +32,8 @@ CHANNELS = [int(ch) if id_pattern.search(
 auth_users = [int(user) if id_pattern.search(
     user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = int(environ.get('AUTH_CHANNEL'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL'))
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(
-    auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
