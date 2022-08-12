2cc3cd034a680e6269a352b9cd930c1df63887e7
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-08-12 20:23:29 +0530

    bug

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -44,40 +44,42 @@ async def gen_link_s(bot, message):
 async def gen_link_batch(bot, message):  # sourcery skip: low-code-quality
     if " " not in message.text:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+
     links = message.text.strip().split(" ")
     if len(links) != 3:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+
     cmd, first, last = links
     regex = re.compile(
         "(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+
     match = regex.match(first)
     if not match:
         return await message.reply('Invalid link')
-    f_chat_id = match.group(4)
-    f_msg_id = int(match.group(5))
+    f_chat_id = match[4]
+    f_msg_id = int(match[5])
     if f_chat_id.isnumeric():
-        f_chat_id = int(("-100" + f_chat_id))
-
+        f_chat_id = int(f"-100{f_chat_id}")
     match = regex.match(last)
     if not match:
         return await message.reply('Invalid link')
-    l_chat_id = match.group(4)
-    l_msg_id = int(match.group(5))
+    l_chat_id = match[4]
+    l_msg_id = int(match[5])
     if l_chat_id.isnumeric():
-        l_chat_id = int(("-100" + l_chat_id))
-
+        l_chat_id = int(f"-100{l_chat_id}")
     if f_chat_id != l_chat_id:
         return await message.reply("Chat ids not matched.")
     try:
         chat_id = (await bot.get_chat(f_chat_id)).id
     except ChannelInvalid:
         return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+
     except (UsernameInvalid, UsernameNotModified):
         return await message.reply('Invalid Link specified.')
     except Exception as e:
         return await message.reply(f'Errors - {e}')
-
     sts = await message.reply("Generating link for your message.\nThis may take time depending upon number of messages")
+
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(
@@ -87,18 +89,16 @@ async def gen_link_batch(bot, message):  # sourcery skip: low-code-quality
     FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
 
     outlist = []
-
-    # file store without db channel
     og_msg = 0
     tot = 0
-    batch_list = bot.get_messages(f_chat_id, [f_msg_id, l_msg_id])
-    logger.info(batch_list)
+    batch_list = bot.get_messages(chat_id, [f_msg_id, l_msg_id])
+    for msg in batch_list:
+        logger.info(msg.id)
     for msg in batch_list:
         tot += 1
         if msg.empty or msg.service:
             continue
         if not msg.media:
-            # only media messages supported.
             continue
         try:
             file_type = msg.media
@@ -107,13 +107,8 @@ async def gen_link_batch(bot, message):  # sourcery skip: low-code-quality
             if caption:
                 caption = caption.html
             if file:
-                file = {
-                    "file_id": file.file_id,
-                    "caption": caption,
-                    "title": getattr(file, "file_name", ""),
-                    "size": file.file_size,
-                    "protect": cmd.lower().strip() == "/pbatch",
-                }
+                file = {"file_id": file.file_id, "caption": caption, "title": getattr(
+                    file, "file_name", ""), "size": file.file_size, "protect": cmd.lower().strip() == "/pbatch"}
 
                 og_msg += 1
                 outlist.append(file)
@@ -121,12 +116,14 @@ async def gen_link_batch(bot, message):  # sourcery skip: low-code-quality
             pass
         if not og_msg % 20:
             try:
-                await sts.edit(FRMT.format(total=l_msg_id-f_msg_id, current=tot, rem=((l_msg_id-f_msg_id) - tot), sts="Saving Messages"))
+                await sts.edit(FRMT.format(total=l_msg_id - f_msg_id, current=tot, rem=l_msg_id - f_msg_id - tot, sts="Saving Messages"))
+
             except Exception:
                 pass
     with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
         json.dump(outlist, out)
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
+
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
     await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
